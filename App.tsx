462e0c453d3e85f8d2cfbc7920dcf6b85e94a51d
Farooq <farooqazammian@gmail.com>
2024-01-31 23:17:29 +0500

    packages/screen added

--- App.tsx
+++ App.tsx
@@ -1,23 +1,16 @@
 import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
-import Buttons from './src/components/buttons/Buttons';
-import axios from 'axios';
+import HomeScreen from './src/screens/HomeScreen';
+import Login from './src/screens/auth/Login';
 
 
 
 export default function App() {
 
-  const getAPI = () => {
-    const baseURL = 'https://dummyjson.com'
-    axios({
-      method: "GET",
-      url: `${baseURL}/todos`
-    }).then(res => console.log(res)).catch(err => console.log(err))
-  }
 
 
   return (
     <SafeAreaView style={styles.container}>
-      <Buttons text={'Get API'} press={getAPI} />
+      <Login />
     </SafeAreaView>
   );
 }
