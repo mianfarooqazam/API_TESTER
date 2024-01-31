27e1d0dfcb2db144b11db3cc8ac54d90333895d9
Farooq <farooqazammian@gmail.com>
2024-01-31 19:34:26 +0500

    Components added

--- App.tsx
+++ App.tsx
@@ -1,12 +1,14 @@
+
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View ,SafeAreaView} from 'react-native';
+import Button from './src/components/Button';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+    <Button text={'GET API'}  />
+      
+    </SafeAreaView>
   );
 }
 
@@ -15,6 +17,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    // justifyContent: 'center',
   },
 });
