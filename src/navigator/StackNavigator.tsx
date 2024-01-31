75007a1efcbbce0bd9912085266dd5eefaabddf5
Farooq <farooqazammian@gmail.com>
2024-02-01 00:31:50 +0500

    Splash Animation added

--- src/navigator/StackNavigator.tsx
+++ src/navigator/StackNavigator.tsx
@@ -5,6 +5,7 @@ import SignUp from '../screens/auth/SignUp';
 import Login from '../screens/auth/Login';
 import ForgotPassword from '../screens/auth/ForgotPassword';
 import HomeScreen from '../screens/HomeScreen';
+import AnimationSplash from '../splash/AnimationSplash';
 
 
 
@@ -13,9 +14,9 @@ const Stack = createNativeStackNavigator();
 const StackNavigator = () => {
   return (
     <NavigationContainer>
-    <Stack.Navigator initialRouteName='HomeScreen'>
+    <Stack.Navigator initialRouteName='AnimationSplash'>
     <Stack.Screen name="HomeScreen" component={HomeScreen} options={{headerShown:false, }}/>
-   
+    <Stack.Screen name="AnimationSplash" component={AnimationSplash} options={{headerShown:false, }}/>
       <Stack.Screen name="SignUp" component={SignUp} options={{headerShown:false, }}/>
       <Stack.Screen name="Login" component={Login} options={{headerShown:false}} />
       <Stack.Screen name="ForgotPassword" component={ForgotPassword} options={{headerShown:false}} />
